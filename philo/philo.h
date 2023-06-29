53af3990840fdd9d56350a77b9839d5cf98f11af
Thunpisis Ponuthai <tponutha@z1t5c2.42bangkok.com>
2023-06-29 22:12:13 +0700

    Save for working at dorm

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 00:20:42 by tponutha          #+#    #+#             */
+/*   Updated: 2023/06/29 00:26:02 by tponutha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <string.h>
+# include <stdio.h>
+# include <pthread.h>
+
+
+#endif
