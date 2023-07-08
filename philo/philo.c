6d99c93402742d22e25af8d88f154b231569b277
Thunpisis Ponuthai <tponutha@z2t18c2.42bangkok.com>
2023-07-08 21:57:40 +0700

    I like Linux:

--- philo/philo.c
+++ philo/philo.c
@@ -6,13 +6,23 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 00:20:35 by tponutha          #+#    #+#             */
-/*   Updated: 2023/07/05 16:59:08 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/07/08 15:41:30 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+/* AV list (ac : 4-5)
+1: philo no
+2: time to die
+3: time to eat
+4: time to sleep
+5: (optional) end no
+*/
+
 int	main(int ac, char **av)
 {
+	if (ac > 5 || ac < 4)
+		return (EXIT_SUCCESS);
 	
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 00:20:42 by tponutha          #+#    #+#             */
-/*   Updated: 2023/07/06 21:58:13 by tponutha         ###   ########.fr       */
+/*   Updated: 2023/07/08 15:40:19 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,24 @@
 
 typedef pthread_mutex_t	t_mutex;
 
+typedef struct s_mem
+{
+	void			*mem;
+	struct s_mem	*next;
+}	t_mem;
+
 typedef struct	s_philo
 {
-	uint32_t	life_t;
-	uint32_t	die_t;
-	uint32_t	eat_t;
-	uint32_t	sleep_t;
-	char		*table;
+	unsigned int	die_t;
+	unsigned int	eat_t;
+	unsigned int	sleep_t;
+	unsigned int	end_no;
+	unsigned int	life_t;
+	unsigned int	*no_die;
+	char			*table;
 }	t_philo;
+
+/*		from libft		*/
+int	ft_atoi(const char *str);
 
 #endif
